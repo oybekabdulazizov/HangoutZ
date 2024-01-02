b56e51453a8998bc1e1aa57c3c15a83f733c084b
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2024-01-02 23:24:05 +0100

    install and set up shadcn-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from './components/ui/button';
+
 const App = () => {
   return (
     <>
       <h1 className='underline text-3xl font-bold'>Vite + React</h1>
+      <Button>Click Me!</Button>
     </>
   );
 };
