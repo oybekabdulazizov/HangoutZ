c63a44350b32e49fdbd07ec59ea6d11352cea10a
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2024-01-06 11:50:46 +0100

    refactor main home component into page/events folder and remove the mock home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 
 import Layout from './components/shared/Layout';
-import Home from './pages/Home';
-
-/* const Home = () => {
-  return (
-    <div className='wrapper'>
-      <h3 className='font-thin text-4xl'>Links:</h3>
-      <div>
-        <ul>
-          <li>
-            <Link to='/auth/sign-up' className='hover:underline'>
-              Sign up
-            </Link>
-          </li>
-          <li>
-            <Link to='/auth/sign-in' className='hover:underline'>
-              Sign in
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
-}; */
+import Home from './pages/events';
 
 const SignUp = () => {
   return <>TODO: Implement Sign Up page</>;
