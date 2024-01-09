c32f744652693c8cdc5e426fd573ac85be5dec30
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2024-01-09 15:42:54 +0100

    implement logging a user out

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -2,8 +2,23 @@ import { FC } from 'react';
 import { Link } from 'react-router-dom';
 
 import { Button } from '@/components/ui/button';
+import useAxiosPrivate from '@/pages/hooks/useAxiosPrivate';
+import useTokens from '@/pages/hooks/useTokens';
 
 const Navbar: FC = ({}) => {
+  const { axiosPrivate } = useAxiosPrivate();
+  const { removeTokens } = useTokens();
+
+  const handleLogout = async () => {
+    try {
+      await axiosPrivate.get('/auth/log-out');
+      removeTokens('session-token');
+      removeTokens('refresh-token');
+    } catch (err: any) {
+      console.log(err);
+    }
+  };
+
   return (
     <header className='w-ful border-b'>
       <div className='wrapper flex items-center justify-between'>
@@ -12,13 +27,16 @@ const Navbar: FC = ({}) => {
           <img src='image.jpg' alt='logo' />
         </Link>
 
-        <div className='flex justify-between items-center w-48'>
+        <div className='flex justify-between items-center w-fit gap-3'>
           <Button className='rounded-full' size='lg'>
             <Link to='/auth/log-in'>Log in</Link>
           </Button>
           <Link to='/auth/sign-up' className='text-black'>
             Sign up
           </Link>
+          <Button className='rounded-full' size='lg' onClick={handleLogout}>
+            Log out
+          </Button>
         </div>
       </div>
     </header>
